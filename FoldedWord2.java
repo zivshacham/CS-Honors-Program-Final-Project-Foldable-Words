794163c759ae4913f60d7c366a048aaa14c1dc5e
AlonLesman <100024954+AlonLesman@users.noreply.github.com>
2022-07-25 17:10:03 +0300

    //

--- FoldedWord2.java
+++ FoldedWord2.java
@@ -1,9 +1,11 @@
+import java.util.LinkedList;
+
 /*
 */
 
 public class FoldedWord2 {
 	public static void main(String[] args) {
-		// Lexicon temp = new Lexicon("words");
+		Lexicon temp = new Lexicon("words");
 		// System.out.println(temp.IsExist("i"));
 		// System.out.println(temp.IsExist("o"));
 		// System.out.println(temp.IsExist("a"));
@@ -19,6 +21,26 @@ public class FoldedWord2 {
 		normalized_str = normalized_str.replaceAll("[^a-z]", "");
 		return normalized_str;
 	}
+	public static void PrintOrderedList(LinkedList<String> lst, String text){
+		// printing the ordered list in groups, based on each word’s starting position within the text.
+		int length = text.length();
+		LinkedList<String>[] table = new LinkedList[length];
+		for(int i = 0; i < length; i++) {
+			table[i] = new LinkedList<String>();
+		}
+		Iterator<String> itr = lst.iterator();
+		while (itr.hasNext()) {
+			String word = itr.next();
+			int index = text.indexOf(word.charAt(0));
+			table[index].add(word); 
+		}
+		for (int j = 0; j < length; j++) {
+			System.out.print("Group " + j + ":");
+			System.out.print(table[j].toString());
+			System.out.println();
+		}
+		System.out.println("There was printed " + lst.size() + " words.");
+	}
 
 	public static void Algo1(){
 
@@ -28,7 +50,25 @@ public class FoldedWord2 {
 		
 	}
 
-	public static void Algo3(){
+	public static LinkedList foldablesByCounting(Lexicon lexicon, String text){
+		text = Normalize(text);
+		int length = text.length();
+		LinkedList words = new LinkedList<String>();
+		for(int i = 0; i < Math.pow(2, length) - 1; i++ ){     //iterate over all the binaray sequance 
+			String BinaryWord = Integer.toBinaryString(i);
+			StringBuilder generatedWord = new StringBuilder();
+			for(int pos = 0; pos < BinaryWord.length() ; pos++){ //this loop generates a new word from binaray sequence
+				if(BinaryWord.charAt(pos)=='1'){                  
+					generatedWord.append(BinaryWord.charAt(pos));
+				}
+			}
+			String word = generatedWord.toString();
+			if (lexicon.IsExist(word)){
+				words.addLast(word);
+			}
+		}
+		PrintOrderedList(words,text);
+		return words;
 		
 	}
 
