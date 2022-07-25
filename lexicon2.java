0bbb693c30ae5ede40b1d403ece82328b81223c8
AlonLesman <100024954+AlonLesman@users.noreply.github.com>
2022-07-25 13:06:22 +0300

    generalUpdate

--- lexicon2.java
+++ lexicon2.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import org.w3c.dom.Node;
+import java.math.*;
 
 /**
  * A library of words lexicon functions.
@@ -20,29 +21,13 @@ public class Lexicon2 {
     * 
     * @param filename words file name
     */
-	public Lexicon2(String filename) {
+	 public Lexicon2(String filename) {
 		this.filename = filename;
 		createLexiconTable(filename);
-        // head = new LinkedList<String>();
-		// head.addLast("I");
-		// head.addLast("a");
-		// head.add("O");
-      	/* try {
-         // try to read from file in working directory
-        	File file = new File (filename);
-        	image  = ImageIO.read (file);
-        	width  = image.getWidth (null);
-        	height = image.getHeight (null);
-      }
-      catch (IOException e) {
-        	throw new RuntimeException ("Could not open file: " + filename);
-      }
-
-      // check that image was read in
-      if (image == null) {
-        	throw new RuntimeException ("Invalid image file: " + filename);
-      }*/
-	// }
+	// 	// head.addLast("I");
+	// 	// head.addLast("a");
+	// 	// head.add("O");
+    
 
 	/**
     * The function gets a word, and return whether the word
@@ -51,9 +36,10 @@ public class Lexicon2 {
     * @param word a string of a word
 	* @return True/False if the word is exist
     */
-	// public boolean IsExist(String word) {
-	// 	return head.contains(word);
-	// }
+	public boolean IsExist(String word) {
+        while(table(hashFunction(word)).
+		return head.contains(word);
+	}
 
 	/**
     * The function gets a word, and return whether the word
@@ -68,29 +54,31 @@ public class Lexicon2 {
 	// 
 
     public LinkedList<String>[] createLexiconTable(String fileName){
-        table = new LinkedList<string>[SIZE];
+        table = new LinkedList<String>[SIZE];
         for (int i = 0; i <= SIZE; i++){
             String word = StdIn.readLine();
             table[hashFunction(word)].add(word);
         }
     }
-    private int hashFunction(String word){
-        long sum;
+    public int hashFunction(String word){
+        BigInteger sum = BigInteger.ZERO;
+        word.toLowerCase();
         for(int i = 0; i < word.length(); i++){
-            toLowerCase(word);
             int c = word.charAt(i);
             c -= 'a'+ 1;
-            c = c * (Math.pow(26,i));
-            sum += c; 
+            c = c *  (int)Math.pow(26,i); 
+            BigInteger charVal = BigInteger.valueOf(c);
+            sum.add(charVal);
         }
-        return sum % ;
+        BigInteger bigSize = BigInteger.valueOf(SIZE);
+        return (sum.remainder(bigSize)).intValue(); // convert bigInt to int  
     }
     
 
 	public static void main(String[] args) {
 		// hash func check
-        String word = "aaa"
-        System.out.prinln("hash func on " + word + ", should print 703. result: \n" + hashFunction(word));
+        String word = "aaa";
+        System.out.println("hash func on  aaa , should print 703. result: \n" + hashFunction("aaa"));
         
 	}
 }
