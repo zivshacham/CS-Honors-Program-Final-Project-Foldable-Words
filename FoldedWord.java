0700f6c406dd2cf8d42b8679e53b7ce57338d0fa
zivshacham <zivsh213@gmail.com>
2022-07-25 17:00:49 +0300

    Update FoldedWord.java

--- FoldedWord.java
+++ FoldedWord.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -8,7 +9,11 @@ public class FoldedWord {
 	public static void main(String[] args) {
 		Lexicon lexicon = new Lexicon("words");
 		String text = "It's a pleasure to serve you!";
-		randomFoldableWords(text, lexicon, 4, 1);
+		System.out.println("Using Alghorithm 'randomFoldableWords':");
+		randomFoldableWords(text, lexicon, 3, 1000000);
+		System.out.println();
+		System.out.println("Using Alghorithm 'FoldableStrings3':");
+		FoldableStrings3(lexicon, text);
 	}
 
 	public static String Normalize(String str){
@@ -18,8 +23,25 @@ public class FoldedWord {
 		return normalized_str;
 	}
 
-	public static void PrintOrderedList(LinkedList<String> lst){
+	public static void PrintOrderedList(LinkedList<String> lst, String text){
 		// printing the ordered list in groups, based on each word’s starting position within the text.
+		int length = text.length();
+		LinkedList<String>[] table = new LinkedList[length];
+		for(int i = 0; i < length; i++) {
+			table[i] = new LinkedList<String>();
+		}
+		Iterator<String> itr = lst.iterator();
+		while (itr.hasNext()) {
+			String word = itr.next();
+			int index = text.indexOf(word.charAt(0));
+			table[index].add(word); 
+		}
+		for (int j = 0; j < length; j++) {
+			System.out.print("Group " + j + ":");
+			System.out.print(table[j].toString());
+			System.out.println();
+		}
+		System.out.println("There was printed " + lst.size() + " words.");
 	}
 
 	public static void randomFoldableWords(String text, Lexicon lexicon, int k, int reps){
@@ -30,18 +52,42 @@ public class FoldedWord {
 		String norm_text = Normalize(text);
 		int length = norm_text.length();
 		LinkedList<String> lst = new LinkedList<>();
+		int[] arr = new int[k];
 		for (int i = 0; i < reps; i++) {
-			int[] arr = new int[k];
+			// the following loop is conducted reps time, for each time constructing array with distinct k indexes
 			Random rnd = new Random();
-			arr = rnd.ints(k, 0, length).sorted().toArray();
-			for (int j = 0; j < k; j++){
-				System.out.println(arr[j]);
+			arr = rnd.ints(k, 0, length).sorted().distinct().toArray(); // generating k random numbers to be the indexes from the text
+			while(arr.length < k) {
+				arr = rnd.ints(k, 0, length).sorted().distinct().toArray(); // generating again if there are less then k indexes
 			}
+			String word = "";
+			for (int j = 0; j < k; j++){
+				word += norm_text.charAt(arr[j]);
+			}
+			if (lexicon.IsExist(word) && !lst.contains(word)) lst.add(word);
 		}
+		PrintOrderedList(lst, norm_text);
 	}
 
-	public static void Algo2(){
-		
+	public static void FoldableStrings3(Lexicon lexicon, String text){
+		/** generates all three-letter strings that can be folded from the given text,
+		*  and returns the subset of those strings that appear in the lexicon given.
+		*/
+		String norm_text = Normalize(text);
+		int length = norm_text.length();
+		LinkedList<String> lst = new LinkedList<>();
+		for (int i = 0; i < length - 2; i++){
+			for (int j = i + 1; j < length - 1; j++){
+				for (int s = i + 2; s < length; s++){
+					String word = "";
+					word += norm_text.charAt(i);
+					word += norm_text.charAt(j);
+					word += norm_text.charAt(s);
+					if (lexicon.IsExist(word) && !lst.contains(word)) lst.add(word);
+				}
+			}
+		}
+		PrintOrderedList(lst, norm_text);
 	}
 
 	public static void Algo3(){
